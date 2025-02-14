de036dccdff431036a5052448f91ae67def0842c
mrjonathanm <mrjonathanmiles@gmail.com>
2025-02-14 00:13:48 +0000

    lkj

--- .eleventy.js
+++ .eleventy.js
@@ -14,3 +14,22 @@ module.exports = function (eleventyConfig) {
     },
   };
 };
+
+const pluginTailwindCSS = require("eleventy-plugin-tailwindcss");
+module.exports = function(eleventyConfig) {
+    eleventyConfig.addPlugin(pluginTailwindCSS);
+};
+
+const autoprefixer = require("autoprefixer");
+module.exports = function (eleventyConfig) {
+  eleventyConfig.addPlugin(eleventySass, {
+    postcss: postcss([autoprefixer]),
+  });
+};
+
+
+const eleventySass = require("eleventy-sass");
+
+module.exports = function (eleventyConfig) {
+  eleventyConfig.addPlugin(eleventySass);
+};
